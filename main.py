232a6c0fee495c0499e7e6fecf55c856d2d00c90
Ilya N <1llyanovohatskyy@gmail.com>
2017-11-16 22:33:10 +0200

    analyze by describing data

--- main.py
+++ main.py
@@ -17,4 +17,23 @@ from sklearn.linear_model  import Perceptron
 from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
 
+SEPARATOR = '_'*40
 
+train_df = pd.read_csv('./input/train.csv')
+test_df = pd.read_csv('./input/test.csv')
+
+combine = [train_df, test_df]
+
+print("Features available in the dataset:\n{}\n{}\n".format(train_df.columns.values, SEPARATOR))
+
+print("Preview the data:\n{}\n{}\n".format(train_df.head(), SEPARATOR))
+
+# data types for various features
+print("\n{}\n{}".format(train_df.info(null_counts=False), SEPARATOR))
+print("\n{}\n{}".format(test_df.info(null_counts=False), SEPARATOR))
+
+# distribution of numerical features
+print("\n{}\n{}".format(train_df.describe(), SEPARATOR))
+
+# distribution of categorical features
+print("\n{}\n{}".format(train_df.describe(include=['O']), SEPARATOR))
